3c464c0193374d49f6c59c40cef8952b1b8bc718
gitlep3 <privserv100@gmail.com>
2023-06-18 07:09:05 -0400

    initial

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -6,7 +6,6 @@ import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-toastify/dist/ReactToastify.css";
 import "./index.scss";
-//test
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
